62403cdefb8a133d21a5bdb92fa4a8929467b0b6
fabricio-roca <fabricio.rocca@outlook.com>
2022-06-21 17:28:32 -0300

    feat: :construction_worker: Implementar nova funções.
    Foi realizado algumas alterações no codigo, deixando o mais limpo, adicionado algumas funções novas.
    
    #2

--- main.js
+++ main.js
@@ -1,55 +1,100 @@
-let nome;
 let numberSecretor;
 let chances;
-let inputChances = document.getElementById("chances");
+let numeroColetado = [];
+let front = {
+  /*
+  *Entrada de Dados */
+  inputs: {
+    chances: document.getElementById("chancesText"),
+    nome: document.getElementById("nome"),
+    nivel: document.getElementsByName("nivel"),
+    numero: document.getElementById("numero"),
+  },
+  /*
+  *Saida de Dados */
+  outputs:{
+    nomeHtml: document.getElementById("nomeText"),
+    chancesHtml: document.getElementById("chancesText"),
+    messageHtml: document.getElementById("messageText"),
+    resultadoHtml: document.getElementById("resultado")
+  }
+};
+let inputChances = document.getElementById("chancesText");
 
-  /* função para nome do jogador */
-  btnNome = function() {
-      nome = document.getElementById("nome").value;
-      alert('Olá ' + nome + ', é um prazer te ver!');
+/*
+  *função para nome do jogador */
+btnNome = function () {
+  front.outputs.nomeHtml.innerHTML = `Olá ${front.inputs.nome.value}, é um prazer te ver!`;
+};
+
+/*
+  *função para a escolha do nivel do jogo */
+btnNivel = function () {
+  let inputNivel = document.getElementsByName("nivel");
+  let nivel;
+
+  for (let i = 0; i < inputNivel.length; i++) {
+    if (inputNivel[i].checked) {
+      nivel = inputNivel[i].value;
+    }
+  }
+  console.log(nivel);
+
+  if (nivel == 1) {
+    chances = 5;
+    numberSecretor = parseInt(Math.random() * 10) + 1;
+    front.outputs.chancesHtml.innerHTML = "Você tem " + chances + " tentativas!";
+    console.log("Numero Sortido", numberSecretor);
+  } else {
+    chances = 10;
+    numberSecretor = parseInt(Math.random() * 20) + 1;
+    front.outputs.chancesHtml.innerHTML = "Você tem " + chances + " tentativas!";
+    console.log("Numero Sortido", numberSecretor);
   }
 
-  /* função para a escolha do nivel do jogo */
-  btnNivel = function(){
-    let inputNivel = document.getElementsByName("nivel");
-    let nivel;
+  let message =
+    front.inputs.nome.value + " escolheu o nivel " + nivel + "!";
+    front.outputs.messageHtml.innerHTML =(message);
+};
 
-    for (let i = 0; i < inputNivel.length; i++){
-      if(inputNivel[i].checked){
-        nivel = inputNivel[i].value;
-      }
-    }
-    console.log(nivel);
+/*
+  *função para começar o jogo */
+btnJogar = function () {
+  console.log("numero coletado array", numeroColetado);
 
-    if(nivel == 1){
-      chances = 5;
-      numberSecretor = parseInt(Math.random() * 11);
-      inputChances.innerHTML = "Você tem " + chances + " tentativas!";
-      console.log(numberSecretor);
+  if (front.inputs.numero.value == numberSecretor) {
+    front.outputs.resultadoHtml.innerHTML = "Você acertou! o numero é " + numberSecretor;
+    console.log("Acertou");
+    fimJogo();
+  }else {
+    if (numeroColetado.includes(front.inputs.numero.value.toString())){
+      front.outputs.chancesHtml.innerHTML = "Você ja escolheu esse numero!";
     }else{
-      chances = 10;
-      numberSecretor = parseInt(Math.random() * 21)
-      inputChances.innerHTML = "Você tem " + chances + " tentativas!";
-      console.log(numberSecretor);
-    }
-
-    let message = (nome +' escolheu o nivel '+ nivel + '! voce tem ' + chances + ' chances.');
-    alert(message);
-  }
-
-  /* função para começar o jogo */
-  btnJogar = function(){
-
-    let number = document.getElementById("numero").value;
-    let resulted = document.getElementById("resultado");
-
-    if (number == numberSecretor) {
-      resulted.innerHTML = "Você acertou! o numero é " + numberSecretor;
-      console.log("Acertou");
-    } else {
-      resulted.innerHTML = "Errou";
+      numeroColetado.push(front.inputs.numero.value);
+      tentativaNumero();
       chances--;
-      inputChances.innerHTML = "Você tem " + chances + " tentativas!";
-      console.log(chances);
-    }
+      front.outputs.chancesHtml.innerHTML = "Você tem " + chances + " tentativas!";
+      }
+    console.log("Numero de chances", chances);
+    if (chances == 0) {
+      fimJogo();
+      inputChances.innerHTML = "Acabou suas chances, você perdeu";
+    };
   }
+}
+
+/*
+ * função fim do jogo */
+fimJogo = function () {
+  let btnDisabled = document.getElementById("inputJogo").disabled = true;
+};
+
+/*
+  * função de maior e menor*/
+tentativaNumero = function(){
+  if (front.inputs.numero.value > numberSecretor){
+    front.outputs.resultadoHtml.innerHTML = "Errou, o numero é menor.";
+  }else{
+    front.outputs.resultadoHtml.innerHTML = "Errou, o numero é maior.";
+  }
+}
