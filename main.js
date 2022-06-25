37677a0669d3feec491b9efb80643e2001ec889c
fabricio-roca <fabricio.rocca@outlook.com>
2022-06-25 00:48:21 -0300

    feat: :art: Finalizar a estrutura logica.
    Foi finalizado a estrutura logica do projeto, iniciando a parte do estilo do jogo.
    
    #3

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ let front = {
     nome: document.getElementById("nome"),
     nivel: document.getElementsByName("nivel"),
     numero: document.getElementById("numero"),
+    resetar: document.getElementById("resetJogo"),
+    cardNome: document.getElementsByClassName('card nome show'),
+    cardNivel: document.getElementsByClassName('card nivel'),
+    cardJogo: document.getElementsByClassName('card jogo')
   },
   /*
   *Saida de Dados */
@@ -16,15 +20,33 @@ let front = {
     nomeHtml: document.getElementById("nomeText"),
     chancesHtml: document.getElementById("chancesText"),
     messageHtml: document.getElementById("messageText"),
-    resultadoHtml: document.getElementById("resultado")
+    resultadoHtml: document.getElementById("resultado"),
+    espacoHtml: document.getElementById("espaco")
   }
 };
 let inputChances = document.getElementById("chancesText");
+
+cardHidden = (elements)=>{
+  let element = elements.item(0).classList.remove('show');
+
+  return element;
+};
+
+cardShow = (elements)=>{
+  let element = elements.item(0).classList.add('show');
+
+  return element;
+};
 
 /*
   *função para nome do jogador */
 btnNome = function () {
   front.outputs.nomeHtml.innerHTML = `Olá ${front.inputs.nome.value}, é um prazer te ver!`;
+
+  cardHidden(front.inputs.cardNome);
+  cardShow(front.inputs.cardNivel);
+
+  return true;
 };
 
 /*
@@ -44,24 +66,26 @@ btnNivel = function () {
     chances = 5;
     numberSecretor = parseInt(Math.random() * 10) + 1;
     front.outputs.chancesHtml.innerHTML = "Você tem " + chances + " tentativas!";
+    front.outputs.espacoHtml.innerHTML = "(1 a 10)";
     console.log("Numero Sortido", numberSecretor);
   } else {
     chances = 10;
     numberSecretor = parseInt(Math.random() * 20) + 1;
     front.outputs.chancesHtml.innerHTML = "Você tem " + chances + " tentativas!";
+    front.outputs.espacoHtml.innerHTML = "(1 a 20)";
     console.log("Numero Sortido", numberSecretor);
   }
 
-  let message =
-    front.inputs.nome.value + " escolheu o nivel " + nivel + "!";
-    front.outputs.messageHtml.innerHTML =(message);
+  let message = front.inputs.nome.value + " escolheu o nivel " + nivel + "!";
+
+  front.outputs.messageHtml.innerHTML =(message);
+
+  cardHidden(front.inputs.cardNivel);
+  cardShow(front.inputs.cardJogo);
 };
 
-/*
-  *função para começar o jogo */
+/** função para começar o jogo */
 btnJogar = function () {
-  console.log("numero coletado array", numeroColetado);
-
   if (front.inputs.numero.value == numberSecretor) {
     front.outputs.resultadoHtml.innerHTML = "Você acertou! o numero é " + numberSecretor;
     console.log("Acertou");
@@ -74,7 +98,8 @@ btnJogar = function () {
       tentativaNumero();
       chances--;
       front.outputs.chancesHtml.innerHTML = "Você tem " + chances + " tentativas!";
-      }
+      console.log("numero coletado array", numeroColetado);
+    }
     console.log("Numero de chances", chances);
     if (chances == 0) {
       fimJogo();
@@ -83,18 +108,21 @@ btnJogar = function () {
   }
 }
 
-/*
- * função fim do jogo */
+/** função fim do jogo */
 fimJogo = function () {
   let btnDisabled = document.getElementById("inputJogo").disabled = true;
 };
 
-/*
-  * função de maior e menor*/
+/** função de maior e menor*/
 tentativaNumero = function(){
   if (front.inputs.numero.value > numberSecretor){
     front.outputs.resultadoHtml.innerHTML = "Errou, o numero é menor.";
   }else{
     front.outputs.resultadoHtml.innerHTML = "Errou, o numero é maior.";
   }
+}
+
+/** função de resetar o jogo */
+btnReset = function(){
+  window.location.reload();
 }
